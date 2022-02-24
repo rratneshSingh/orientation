f44eeb7775b17f7d3198b65d98e6b1a03ad99b97
ratnesh singh <dev.ratnesh.singh@gmail.com>
2022-02-24 23:18:11 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'orientation';
+
+  oneTimeAccessOrientationAngle: number = 0;
+  oneTimeAccessOrientationType: string = '';
+
+  changingOrientationAngle: number = 0;
+  changingOrientationType: string = '';
+
+  constructor(){}
+
+  ngOnInit() {
+    this.oneTimeAccessOrientationAngle = window.screen.orientation.angle;
+    this.oneTimeAccessOrientationType = window.screen.orientation.type;
+    window.screen.orientation.onchange = this.callback as () => any;
+  }
+ 
+  callback( orientation: ScreenOrientation, ev: Event ) {
+    this.changingOrientationAngle = orientation.angle;
+    this.changingOrientationType = orientation.type;
+  };
 }
